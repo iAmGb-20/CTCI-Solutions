3c9b21961f1857181b45a3a11646a8a496c9bb4c
iAmGb-20 <gchille@rett-imac-01.ur.rochester.edu>
2020-10-12 16:20:06 -0400

    Completed

--- Arrays_and_Strings/isUnique.java
+++ Arrays_and_Strings/isUnique.java
@@ -1,1 +1,44 @@
+public class isUnique
+{
+    //Test case = "abcde"-->return true
+    //Test case 2 = "aabcde"--> return false
 
+    //helper function..to help compare two characters
+    public boolean compareChar (char a, char b)
+    {
+        if (a == b)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public boolean isunique(String s)
+    {
+        
+        for (int i = 0; i < s.length()-1; i++)
+        {
+            char a = s.charAt(i);
+            char b = s.charAt(i+1);
+            //System.out.println("current a is: " + a + "\n" + "and current b is: " + b+  "\n");
+            if (!compareChar(a, b))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[])
+    {
+        isUnique tester = new isUnique();
+        String s = "abcde";
+        String t = "aabce";
+        System.out.println("Boolean value is: " + tester.isunique(s));
+        System.out.println("Boolean value is: " + tester.isunique(t));
+    }
+
+}
