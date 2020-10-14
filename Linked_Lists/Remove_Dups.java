7d42935eed9053982afc437c5935233cb2918d3e
iAmGb-20 <gchille@u.rochester.edu>
2020-10-14 19:57:16 -0400

    works

--- Linked_Lists/Remove_Dups.java
+++ Linked_Lists/Remove_Dups.java
@@ -1,3 +1,73 @@
+import java.util.*;
+
+public class LinkedList
+{
+    Node head;
+
+    class Node 
+    {
+        Node next = null;
+        int data;
+    }
+}
 public class Remove_Dups.java
 {
+    /*
+    Remove duplicates from an unsorted linked list
+    --using a hashtable
+    what type of linked list is this? Integer?
+    Test case: a->b->c->a..Output: a->b->c
+                a->b->b->c->d ..Output: a->b->c->d
+    Procedure:
+        Iterate the linked list
+        If I see an element, I add it to the hashtable only if it is not there
+        If it is there, I change the pointers to make sure the previous element points to the element next to the repeated one
+        helper functions?? How will I move the pointers??r
+    */
+    
+    public boolean remove_dups (Node head)
+    {
+        Hashset<Integer> set = new Hashset<Integer>();
+        Node prev = null;
+        while (head != null)
+        {
+            if (!set.contains(head.data))
+            {
+                set.add(head.data);
+                prev = head;
+            }
+            else
+            {
+                prev.next = head.next;
+            }
+            head = head.next;
+        }
+        /*
+        This is my algorithm...
+        Hashtable<Integer, Integer> checker = new Hashtable<Integer, Integer>();
+        Node temp = head;
+        Node prev = null;
+        while (temp != null)
+        {
+            if (!checker.containsKey(temp.data))
+            {
+                checker.put(temp.data, i);
+                prev = temp;
+                temp = temp.next;
+            }
+            else
+            {
+                prev.next = temp.next;
+                temp = temp.next;
+            }
+        }
+        Code would take O(n) time whereby n is the number of elements in the list
+        
+        */
+
+
+    }
+
+
+
 }
