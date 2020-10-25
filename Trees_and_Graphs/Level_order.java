00adda736dffc0bbbd6e9ac0d7719951afffb953
iAmGb-20 <gchille@u.rochester.edu>
2020-10-25 13:31:44 -0400

    fixed bugs

--- Trees_and_Graphs/Level_order.java
+++ Trees_and_Graphs/Level_order.java
@@ -1,10 +1,10 @@
 import java.util.*;
 public class Level_order extends BST
 {
-    public void LevelOrder(BST root)
+    public void LevelOrder(BSTNode root)
     {
         if (root == null) return;
-        Queue<BSTNode> Q = new Queue<BSTNode>();
+        Queue<BSTNode> Q = new LinkedList<BSTNode>();
         Q.offer(root);
         while (!Q.isEmpty())
         {
@@ -12,27 +12,27 @@ public class Level_order extends BST
             System.out.println("Current data is: " + current.data);
             if (current.left != null) Q.offer(current.left);
             if (current.right != null) Q.offer(current.right);
-            Q.pop();
+            Q.remove();
         }
         
     }
 
-    public List<<List<Integer>> levelorder (BSTNode root)
+    public List<List<Integer>>levelorder (BSTNode root)
     {
-        List<List<Integer>> lorder = new ArrayList<List<Integer>>();
+        List<List<Integer>> lorder = new LinkedList<List<Integer>>();
         if (root == null) return lorder; //return an empty list in case the root is null
-        Queue<BSTNode> toDiscover = new Queue<BSTNode>();
+        Queue<BSTNode> toDiscover = new LinkedList<BSTNode>();
         toDiscover.offer(root);
 
         while(!toDiscover.isEmpty())
         {
             ArrayList<Integer>subList = new ArrayList<Integer>();
-            int currsize = Q.size();
+            int currsize = toDiscover.size();
             for (int i = 0; i < currsize; i++)
             {
-                if (Q.peek().left != null) Q.offer(Q.peek().left);
-                if (Q.peek().right != null) Q.offer(Q.peek().right);
-                subList.add(Q.poll().data);
+                if (toDiscover.peek().left != null) toDiscover.offer(toDiscover.peek().left);
+                if (toDiscover.peek().right != null) toDiscover.offer(toDiscover.peek().right);
+                subList.add(toDiscover.poll().data);
             }
             lorder.add(subList);
         }
