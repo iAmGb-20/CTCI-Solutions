cef24b791594cfec94af6d14ff2d6b3b4eb80583
iAmGb-20 <gchille@rett-imac-01.ur.rochester.edu>
2020-10-26 19:39:40 -0400

    string compression is cool

--- Arrays_and_Strings/String_compression.java
+++ Arrays_and_Strings/String_compression.java
@@ -1,3 +1,36 @@
-public class String_compression {
-    
+public class String_compression 
+{
+    /*
+    Give a string such as aabcccccaaa...I should return the no of occurences for each character
+    return a2b1c5a3
+    if characters are unique then just return the original string
+    string has both uppercase and lower case letters
+
+    Procedure:
+    - use a string builder
+
+    */
+
+    public String compress (String k)
+    {
+        StringBuilder compressed = new StringBuilder();
+        int count = 0;
+        for (int i = 0; i < k.length(); i++)
+        {
+            count++;
+            if (k.charAt(i) != k.charAt(i+1)|| i+1 >= k.length())
+            {
+                compressed.append(k.charAt(i));
+                compressed.append(count);
+                count = 0;
+            }
+          
+        }
+        if (compressed.length() < k.length())
+        {
+            return compressed.toString();
+        }
+        return k;
+    }
+
 }
