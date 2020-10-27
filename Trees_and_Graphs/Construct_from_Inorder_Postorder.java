7a2b4734c4c8d1813430a032b8b4458065a1fdf9
iAmGb-20 <gchille@u.rochester.edu>
2020-10-27 15:14:35 -0400

    really good leetcode problem

--- Trees_and_Graphs/Construct_from_Inorder_Postorder.java
+++ Trees_and_Graphs/Construct_from_Inorder_Postorder.java
@@ -1,3 +1,40 @@
 public class Construct_from_Inorder_Postorder {
     
+ // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution 
+    {
+        public TreeNode buildTree(int[] inorder, int[] postorder) {
+            return constructTreeFromInPost(inorder, postorder, 0, inorder.length-1, postorder.length-1);
+        }
+        private TreeNode constructTreeFromInPost(int[] inorder, int[] postorder, int start, int end, int index)
+        {
+            if (start > end)
+            {
+                return null;
+            }
+            TreeNode root = new TreeNode(postorder[index]);
+            int inRootIndex = start;
+            while (postorder[index] != inorder[inRootIndex])
+            {
+                inRootIndex++;
+            }
+            root.right = constructTreeFromInPost(inorder, postorder, inRootIndex+1, end, index-1);
+            root.left = constructTreeFromInPost(inorder, postorder, start, inRootIndex-1, index-(end-inRootIndex)-1);
+            return root;
+        
+        }
+    }
 }
