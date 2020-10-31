e64f6593944142c12117e64a1e4047db8e95705b
Chille <gchille@rett-imac-18.ur.rochester.edu>
2020-10-31 18:07:40 -0400

    added Search range

--- Leetcode/BinarySearch/SearchRange.java
+++ Leetcode/BinarySearch/SearchRange.java
@@ -6,18 +6,49 @@ public class SearchRange
     */
     public int[] search (int[] nums, int target)
     {
-        int n = nums.length;
+        int[] result = new int[2];
+        result[0] = findFirst(nums, target);
+        result[1] = findLast(nums, target);
+        return result;
+    }
+    private int findFirst(int[] nums, int target)
+    {
+        int index = -1;
         int start = 0;
-        int end = n-1;
-
-        while (start < end)
+        int end = nums.length-1;
+        while (start <= end)
         {
-            int mid = start + (end-start)/2; //to avoid integer overflow
-            if (nums[mid] > target)
+            int mid = start + (end-start)/2;
+            if (nums[mid] >= target)
             {
-                
+                end = mid -1;
             }
-
+            else
+            {
+                start = mid + 1;
+            }
+            if (nums[mid] == target) 
+            {
+                index = nums[mid];
+            }
         }
+        return index;
+    }
+    private int findLast(int[] nums, int target)
+    {
+        int mid = start + (end-start)/2;
+        if (nums[mid] <= target)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        if (nums[mid] == target)
+        {
+            index = nums[mid];
+        }
+        return index;
     }
 }
